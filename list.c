9ae74c7d256a4b58023fad218e5f2c4f19d3d54a
EduardoPerezMir <eduardo.perez.m@mail.pucv.cl>
2023-06-04 00:51:06 +0000

    Primera implementación TDA lista

---
+++ list.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "list.h"
+
+typedef struct Node Node;
+
+struct Node {
+    void * data;
+    Node * next;
+    Node * prev;
+};
+
+struct List {
+    Node * head;
+    Node * tail;
+    Node * current;
+};
+
+typedef List List;
+
+Node * createNode(void * data) {
+    Node * new = (Node *)malloc(sizeof(Node));
+    assert(new != NULL);
+    new->data = data;
+    new->prev = NULL;
+    new->next = NULL;
+    return new;
+}
+
+List * createList() {
+  List *listaEnlazada = (List *) malloc(sizeof(List));
+  if (listaEnlazada == NULL)
+      return NULL;
+  
+  listaEnlazada->head = NULL;
+  listaEnlazada->tail = NULL;
+  listaEnlazada->current = NULL;
+  
+  return listaEnlazada;
+}
+
+void * firstList(List * list) {
+  if (list->head == NULL)
+    return NULL;
+  
+  list->current = list->head;
+  
+  return list->current->data;
+}
+
+void * nextList(List * list) {
+  if (list->current == NULL || list->current->next == NULL)
+    return NULL;
+  
+  list->current = list->current->next;
+  
+  return list->current->data;
+}
+
+void * lastList(List * list) {
+  if (list->tail == NULL)
+    return NULL;
+  
+  list->current = list->tail;
+  
+    return list->current->data;
+}
+
+void * prevList(List * list) {
+  if (list->current == NULL || list->current->prev == NULL)
+    return NULL;
+
+  list->current = list->current->prev;
+  
+  return list->current->data;
+}
+
+void pushBack(List * list, void * data) {
+    Node *nuevoNodo = createNode(data);
+    
+    if (list->head == NULL)
+    {
+        nuevoNodo->next = nuevoNodo;
+        nuevoNodo->prev = nuevoNodo;
+        list->head = nuevoNodo;
+        list->tail = nuevoNodo;
+    }
+    else
+    {
+        list->current = list->tail;
+        list->tail->next = nuevoNodo;
+        list->tail = nuevoNodo;
+        nuevoNodo->prev = list->current;
+    }
+}
+
+void * popFront(List * list) {
+    list->current = list->head;
+    return popCurrent(list);
+}
+
+void * popBack(List * list) {
+    list->current = list->tail;
+    return popCurrent(list);
+}
+
+void * popCurrent(List * list) {
+  if (list->head == NULL)
+    return NULL;
+  
+  void *dato = NULL;
+  dato = list->current->data;
+  
+  if (list->current == list->tail)
+  {
+    list->tail = list->tail->prev;
+    list->tail->next = NULL;
+  }
+  else if (list->current == list->head)
+  {
+    list->head = list->head->next;
+    list->head->prev = NULL;
+  }
+  else
+  {
+    list->current->prev->next = list->current->next;
+    list->current->next->prev = list->current->prev;
+  }
+  
+  free(list->current);
+  
+  list->current = list->head;
+  
+  return dato;
+}
+
+void cleanList(List * list) {
+    while (list->head != NULL) {
+        popFront(list);
+    }
+}
--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include "list.h"
+
 
 int main(void) {
   printf("Hello World\n");
